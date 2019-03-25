399d7009df66c6021b54661cad115aab10800609
radigin <VYRadygin@mephi.ru>
2019-03-25 14:29:15 +0300

    Убрано HTTP-only для сессии

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_inf_sec_tester_session'
+Rails.application.config.session_store :cookie_store, 
+  key: '_inf_sec_tester_session', httponly: false
